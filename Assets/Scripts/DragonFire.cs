496ac003bcb3a5839d3e13f71f304a1b6de73a08
Stephen <sjgowen@gmail.com>
2025-07-03 21:20:47 +0100

    Got a flame effect by utilising the particle effects, not very impressed so looked at others.
    I downloaded the FreeQuickEffects by Gabriel Aguiar. This effect looks impressive.

--- Assets/Scripts/DragonFire.cs
+++ Assets/Scripts/DragonFire.cs
@@ -2,19 +2,15 @@ using UnityEngine;
 
 public class DragonFire : MonoBehaviour
 {
-    public GameObject dragonFire;
-    public float launchVelocity = 1200f;
+    public GameObject dragonFireBreath;
 
     public void ShootFromMouth()
     {
-        if (dragonFire != null)
+        if (dragonFireBreath != null)
         {
-            GameObject fire = Instantiate(dragonFire, transform.position, transform.rotation);
-            Rigidbody rb = fire.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.AddForce(transform.forward * launchVelocity);
-            }
+            //Debug.Log("Dragon is breathing fire!");
+            GameObject breath = Instantiate(dragonFireBreath, transform.position, transform.rotation);
+            Destroy(breath, 3.0f);
         }
     }
 }
